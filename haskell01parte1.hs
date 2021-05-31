3a34c341ac2847b90ff2ee395c4706566f9b93cf
Ivan Maidana <>
2021-05-31 19:58:28 +0000

    Fiz todos os exercicios propostos

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,2 +1,32 @@
 -- Prática 01 de Haskell - Parte 1
--- Nome: 
+-- Nome: Ivan Maidana da Silveira
+
+sumSquares :: Int -> Int -> Int
+sumSquares x y = (x^2) + (y^2)
+
+circleArea :: Float -> Float
+circleArea r = r * pi 
+
+age :: Int -> Int -> Int
+age ano_nasc ano_atual = ano_atual - ano_nasc
+
+isElderly :: Int -> Bool
+isElderly idade = idade > 65 
+
+htmlItem :: String -> String
+htmlItem x = "<li>"++ x ++"<lis>" 
+
+startsWithA :: String -> Bool
+startsWithA x = (head x) == 'A'
+
+isVerb :: String -> Bool
+isVerb y = (last y) == 'r'
+
+isVowel :: Char -> Bool
+isVowel x = elem x "a,e,i,o,u"
+
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads x y = (head x) == (head y)
+
+isVowel2 :: Char -> Bool
+isVowel2 x = elem x "a, A, e, E, i, I, o, O, u, U"
--- haskell01parte3/svgSimple.hs
+++ haskell01parte3/svgSimple.hs
@@ -19,6 +19,13 @@ svgAll =
   svgBegin 500 500 ++ 
   (svgCircle 60 60 50 "rgb(10, 145, 32, 0.4)") ++ 
   (svgCircle 90 90 50 "rgb(105, 14, 30, 0.4)") ++ 
+  (svgCircle 120 120 30 "rgb(95, 12, 31, 04)") ++
+  (svgCircle 400 400 89 "rgb(95, 205, 31, 04)") ++
+  (svgCircle 00 00 15 "rgb(199, 205, 31, 09)") ++ --direita cima
+  (svgCircle 500 500 15 "rgb(199, 205, 31, 09)") ++ --esquerda baixo
+  (svgCircle 500 00 15 "rgb(199, 205, 31, 09)") ++ 
+  (svgCircle 00 500 15 "rgb(199, 205, 31, 09)") ++
+  (svgCircle 200 200 54 "rgb(199, 34, 31, 19)") ++
   svgEnd
 
 main :: IO ()
